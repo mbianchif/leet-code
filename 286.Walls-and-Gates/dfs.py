be8f5a2e245ed919f3642c26beb544308196c44a
mbianchif <89595589+mbianchif@users.noreply.github.com>
2025-08-06 00:10:42 -0300

    graph

--- 286.Walls-and-Gates/dfs.py
+++ 286.Walls-and-Gates/dfs.py
@@ -1,26 +1,26 @@
+from collections import deque
+
+
 class Solution:
     def islandsAndTreasure(self, grid: list[list[int]]) -> None:
-        n = len(grid)
-        m = len(grid[0])
+        dxs = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        n, m = len(grid), len(grid[0])
+        inf = 2147483647
 
-        def in_range(i, j):
-            return i >= 0 and j >= 0 and i < n and j < m
-
-        def traverse(i, j, count):
-            if not in_range(i, j):
-                return
-
-            if grid[i][j] < count:
-                return
-
-            grid[i][j] = count
-            traverse(i - 1, j, count + 1)
-            traverse(i + 1, j, count + 1)
-            traverse(i, j - 1, count + 1)
-            traverse(i, j + 1, count + 1)
+        def adjacents(v):
+            for di, dj in dxs:
+                ai, aj = v[0] + di, v[1] + dj
+                if 0 <= ai < n and 0 <= aj < m and grid[ai][aj] == inf:
+                    yield ai, aj
 
         # O(n * m)
-        for i in range(n):
-            for j in range(m):
-                if grid[i][j] == 0:
-                    traverse(i, j, 0)
+        q = deque((i, j) for i in range(n) for j in range(m) if grid[i][j] == 0)
+
+        # O(n * m)
+        while q:
+            v = q.popleft()
+
+            for w in adjacents(v):
+                if grid[w[0]][w[1]] == inf:
+                    grid[w[0]][w[1]] = grid[v[0]][v[1]] + 1
+                    q.append(w)
